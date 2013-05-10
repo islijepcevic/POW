9925644d890a82524390504d8fc8c44e4a5f1043
islijepcevic <islijepcevic@gmail.com>
2013-05-10 14:55:26 +0200

    added serialization to Particle

--- PSO/Particle.hpp
+++ PSO/Particle.hpp
@@ -10,12 +10,33 @@
 #define __PARTICLE__
 
 #include <vector>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
 
 #define VERY_LARGE_VALUE 100000000
 
 namespace PSO {
 
 class Particle {
+private:
+    // BOOST SERIALIZATION
+    friend class boost::serialization::access;
+
+    /*
+     * method that for serializing and deserializing objects of this class
+     * @param ar - archive stream
+     * @param version - don't know, not used
+     */
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int version) {
+        ar & currentPosition;
+        ar & currentVelocity;
+        ar & currentValue;
+        ar & bestPosition;
+        ar & bestValue;
+    }
+
 public:
 
     // variables associated to current solution

e6cf8b0dfe43334b18807164c6b41118f80698df
islijepcevic <islijepcevic@gmail.com>
2013-05-17 18:40:44 +0200

    AbstractSpace mostly commented out to perform basic test

--- PSO/AbstractSpace.cpp
+++ PSO/AbstractSpace.cpp
@@ -10,46 +10,46 @@
 
 namespace PSO {
 
-AbstractSpace::AbstractSpace(const std::vector<Dimension*>& _dimensions) :
-    dimensions(_dimensions) {
-
-}
+//AbstractSpace::AbstractSpace(const std::vector<Dimension*>& _dimensions) :
+//    dimensions(_dimensions) {
+//
+//}
 
 AbstractSpace::~AbstractSpace() {
 }
 
-int AbstractSpace::getNoDimensions() const {
-    return dimensions.size();
-}
-
-double AbstractSpace::getLowerBound(int dimensionIndex) {
-    return dimensions[dimensionIndex].getLowerBound();
-}
-
-double AbstractSpace::getHigherBound(int dimensionIndex) {
-    return dimensions[dimensionIndex].getHigherBound();
-}
-
-double AbstractSpace::getSize(int dimensionIndex) {
-    return dimensions[dimensionIndex].getSize();
-}
-
-void AbstractSpace::checkBoundaries(Particle& particle) const {
-    // TODO
-}
-
-void AbstractSpace::calculateDistanceVector(const Particle& particle1,
-                                        const Particle& particle2) const {
-    // TODO
-    //
-    // for every dimension, get the distance between two particles
-}
-
-void AbstractSpace::calculateShortestDistanceVector(const Particle& particle1,
-                                        const Particle& particle2) const {
-    // TODO
-    //
-    // for every dimension, get the shortest distance between two particles
-}
-
-} // namespace PSO
+//int AbstractSpace::getNoDimensions() const {
+//    return dimensions.size();
+//}
+//
+//double AbstractSpace::getLowerBound(int dimensionIndex) {
+//    return dimensions[dimensionIndex].getLowerBound();
+//}
+//
+//double AbstractSpace::getHigherBound(int dimensionIndex) {
+//    return dimensions[dimensionIndex].getHigherBound();
+//}
+//
+//double AbstractSpace::getSize(int dimensionIndex) {
+//    return dimensions[dimensionIndex].getSize();
+//}
+//
+//void AbstractSpace::checkBoundaries(Particle& particle) const {
+//    // TODO
+//}
+//
+//void AbstractSpace::calculateDistanceVector(const Particle& particle1,
+//                                        const Particle& particle2) const {
+//    // TODO
+//    //
+//    // for every dimension, get the distance between two particles
+//}
+//
+//void AbstractSpace::calculateShortestDistanceVector(const Particle& particle1,
+//                                        const Particle& particle2) const {
+//    // TODO
+//    //
+//    // for every dimension, get the shortest distance between two particles
+//}
+//
+//} // namespace PSO
--- PSO/AbstractSpace.hpp
+++ PSO/AbstractSpace.hpp
@@ -10,23 +10,33 @@
 #define __ABSTRACT_SPACE__
 
 #include <vector>
-#include "Dimension.hpp"
+//#include "Dimension.hpp"
 
 namespace PSO {
 
 class AbstractSpace {
 private:
 
-    // this is for the description of dimensions
-    const std::vector<Dimension*>& dimensions;
+//    // this is for the description of dimensions
+//    const std::vector<Dimension*>& dimensions;
 
 public:
 
     /*
-     * constructor
-     * empty implementation provided
+     * THIS CONSTRUCTOR IS JUST FOR THE DUMMY ABSTRACT SPACE, FOR TESTING AND
+     * DEVELOPING PURPOSES
+     * WITH THIS CONSTRUCTOR, THE MAJORITY OF THE IMPLEMENTATION IS STILL IN THE
+     * PYTHON CODE
+     *
+     * DELETE THIS CONSTRUCTOR WHEN THE OTHER ONE COMES IN ACTION
      */
-    AbstractSpace(const std::vector<Dimension*>& _dimensions);
+    AbstractSpace() {}
+
+//    /*
+//     * constructor
+//     * empty implementation provided
+//     */
+//    AbstractSpace(const std::vector<Dimension*>& _dimensions);
 
     /*
      * destructor
@@ -34,49 +44,49 @@ public:
      */
     virtual ~AbstractSpace();
 
-    /*
-     * getter for number of dimensions
-     * @return - number of dimensions
-     */
-    int getNoDimensions() const;
-
-    /*
-     * getter for lower bound of wanted dimension
-     * @param dimensionIndex - index of the wanted dimension
-     * @return - lower bound for given dimension
-     */
-    double getLowerBound(int dimensionIndex);
-
-    /*
-     * getter for higher bound of wanted dimension
-     * @param dimensionIndex - index of the wanted dimension
-     * @return - higher bound for given dimension
-     */
-    double getHigherBound(int dimensionIndex);
-
-    /*
-     * getter for lower bound of dimension d
-     * @param dimensionIndex - index of the wanted dimension
-     * @return - lower bound for given dimension
-     */
-    double getSize(int dimensionIndex);
-
-    /*
-     *
-     */
-    void checkBoundaries(Particle& particle) const;
-
-    /*
-     * do not return void
-     */
-    void calculateDistanceVector(const Particle& particle1,
-                            const Particle& particle2) const;
-
-    /*
-     * do not return void
-     */
-    void calculateShortestDistanceVector(const Particle& particle1,
-                            const Particle& particle2) const;
+//    /*
+//     * getter for number of dimensions
+//     * @return - number of dimensions
+//     */
+//    int getNoDimensions() const;
+//
+//    /*
+//     * getter for lower bound of wanted dimension
+//     * @param dimensionIndex - index of the wanted dimension
+//     * @return - lower bound for given dimension
+//     */
+//    double getLowerBound(int dimensionIndex);
+//
+//    /*
+//     * getter for higher bound of wanted dimension
+//     * @param dimensionIndex - index of the wanted dimension
+//     * @return - higher bound for given dimension
+//     */
+//    double getHigherBound(int dimensionIndex);
+//
+//    /*
+//     * getter for lower bound of dimension d
+//     * @param dimensionIndex - index of the wanted dimension
+//     * @return - lower bound for given dimension
+//     */
+//    double getSize(int dimensionIndex);
+//
+//    /*
+//     *
+//     */
+//    void checkBoundaries(Particle& particle) const;
+//
+//    /*
+//     * do not return void
+//     */
+//    void calculateDistanceVector(const Particle& particle1,
+//                            const Particle& particle2) const;
+//
+//    /*
+//     * do not return void
+//     */
+//    void calculateShortestDistanceVector(const Particle& particle1,
+//                            const Particle& particle2) const;
 
 };
 

b735ebc92ccdf161c920febe2e0a62b29ecd951f
islijepcevic <islijepcevic@gmail.com>
2013-05-25 02:04:39 +0200

    deprecated compile.sh updated (not fully)

--- PSO/compile.sh
+++ PSO/compile.sh
@@ -6,16 +6,16 @@ swig -c++ -python -I/usr/lib/python2.7/site-packages/mpi4py/include PSO.i
 echo "COMPILE MY CPP"
 mpic++ -O2 -fPIC -c AbstractSpace.cpp          #compiled
 mpic++ -O2 -fPIC -c PsoParameters.cpp         #compiled
-#mpic++ -O2 -fPIC -c Dimension.cpp             #compiled
+mpic++ -O2 -fPIC -c Dimension.cpp             #compiled
 #mpic++ -O2 -fPIC -c GeometricNeighbourhood.cpp
 #mpic++ -O2 -fPIC -c GlobalNeighbourhood.cpp   
 #mpic++ -O2 -fPIC -c IndexedNeighbourhood.cpp  
 #mpic++ -O2 -fPIC -c NeighbourhoodFactory.cpp  
 mpic++ -O2 -fPIC -c Particle.cpp              #compiled
-#mpic++ -O2 -fPIC -c PeriodicDimension.cpp     
+mpic++ -O2 -fPIC -c PeriodicDimension.cpp     
 mpic++ -O2 -fPIC -c PSO.cpp                   #compiled
 mpic++ -O2 -fPIC -c randomUtils.cpp           #compiled
-#mpic++ -O2 -fPIC -c ReflexiveDimension.cpp    
+mpic++ -O2 -fPIC -c ReflexiveDimension.cpp    
 #mpic++ -O2 -fPIC -c Swarm.cpp                 
 
 echo "COMPILE SWIG CPP"

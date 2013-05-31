c80cbbd6115693ba397804847277821dfe49022f
islijepcevic <islijepcevic@gmail.com>
2013-05-31 20:38:58 +0200

    minor changes in constants.hpp

--- PSO/constants.hpp
+++ PSO/constants.hpp
@@ -7,5 +7,10 @@
  * e-mail: ivan.slijepcevic@epfl.ch
  */
 
+#ifndef __CONSTANTS__
+#define __CONSTANTS__
+
 // large value, mainly for initializing particles' value
 const double VERY_LARGE_VALUE = 100000000.0;
+
+#endif // __CONSTANTS__

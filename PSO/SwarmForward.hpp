1b90ebdbb19b49d8c53aa19634df7fd1a4e6c29b
islijepcevic <islijepcevic@gmail.com>
2013-05-31 20:32:11 +0200

    forward declaration of Swarm; (if n'hood member of swarm - but it's not)

---
+++ PSO/SwarmForward.hpp
@@ -0,0 +1,18 @@
+/*
+ * SwarmForward.hpp
+ *
+ * forward declaration of Swarm class
+ *
+ * author: Ivan Slijepcevic
+ * e-mai: ivan.slijepcevic@epfl.ch
+ */
+#ifndef __SWARM_FORWARD__
+#define __SWARM_FORWARD__
+
+namespace PSO {
+
+class Swarm;
+
+} // namespace PSO
+
+#endif // __SWARM_FORWARD__

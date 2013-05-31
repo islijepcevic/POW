e814880ac40ad85e9ab4e0926ca9316062465ae6
islijepcevic <islijepcevic@gmail.com>
2013-05-31 20:30:39 +0200

    implemented indexed n'hood (not good yet)

--- PSO/IndexedNeighbourhood.cpp
+++ PSO/IndexedNeighbourhood.cpp
@@ -8,21 +8,75 @@
  */
 #include "IndexedNeighbourhood.hpp"
 
+#include "constants.hpp"
+#include "Swarm.hpp"
+
+#include <cstdio>
+#include <iostream>
+
 namespace PSO {
 
-IndexedNeighbourhood::IndexedNeighbourhood(const Swarm& _swarm) :
-        Neighbourhood(_swarm) {
+IndexedNeighbourhood::IndexedNeighbourhood() {
+}
+
+IndexedNeighbourhood::IndexedNeighbourhood(int nParticles, int _nhoodSize) :
+    bestNeighbours(),
+    nhoodSize(_nhoodSize) {
+
+    bestNeighbours.reserve(nhoodSize);
+
+    if (2*nhoodSize + 1 > nParticles) {
+        nhoodSize = (nParticles - 1) / 2;
+    }
 }
 
 IndexedNeighbourhood::~IndexedNeighbourhood() {
 }
 
-void IndexedNeighbourhood::scanNeighbours() {
-    // TODO iterate through all the swarm, finding the best particle
+void IndexedNeighbourhood::scanNeighbours(const Swarm& swarm) {
+
+    int noParticles = swarm.getNoParticles();
+    for (int partIndex = 0; partIndex < noParticles; partIndex++) {
+
+        printf("PETLJAM PO PARTICLIMA %d\n", partIndex);
+
+        int startIndex = (partIndex - nhoodSize);
+        int endIndex = (partIndex + nhoodSize);
+
+        int bestIndex = startIndex;
+        double bestValue = VERY_LARGE_VALUE;
+
+        // search the neighbourhood
+        for (int iter = startIndex; iter <= endIndex; iter++) {
+            // this needs to be done like that because % operator in c++ is 
+            // defined like shit (and even changes with different compilers)
+            int index = (iter + noParticles) % noParticles;
+            double value = swarm.getParticle(index).bestValue;
+            
+            if (value < bestValue)  {
+                bestValue = value;
+                bestIndex = index;
+            }
+        }
+
+        printf("SAD ME NAMJESTI\n");
+        printf("pos %d\nvel %d\nval %lf\nbps %d\nbvl %lf\n",
+                swarm.getParticle(bestIndex).currentPosition.size(),
+                swarm.getParticle(bestIndex).currentVelocity.size(),
+                swarm.getParticle(bestIndex).currentValue,
+                swarm.getParticle(bestIndex).bestPosition.size(),
+                swarm.getParticle(bestIndex).bestValue
+      );
+
+        bestNeighbours[partIndex] = swarm.getParticle(bestIndex);
+        printf("OCU DALJE\n");
+    }
+    printf("OCU VAN\n");
 }
 
-Particle& IndexedNeighbourhood::findBestNeighbour(int particleIndex) const {
-    // TODO
+const Particle& IndexedNeighbourhood::findBestNeighbour(int particleIndex) const
+{
+    return bestNeighbours[particleIndex];
 }
 
 } // namespace PSO
--- PSO/IndexedNeighbourhood.hpp
+++ PSO/IndexedNeighbourhood.hpp
@@ -11,23 +11,64 @@
 
 #include "Neighbourhood.hpp"
 
+#include <vector>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/base_object.hpp>
+
 namespace PSO {
 
+/**
+ * Indexed neighbourhood is a type of neighbourhood in which all particles are
+ * stored in a list/array, and then the neighbours in this list, with respect to
+ * the index to the array, are considered.
+ */
 class IndexedNeighbourhood : public Neighbourhood {
 private:
 
-    // TODO
+    /**
+     * for every particle, store the best particle in the neighbourhood
+     */
+    std::vector<Particle> bestNeighbours;
+
+    /**
+     * this number represents the size of neighbourhood in one direction
+     * that means, the total neighbourhood size will be:
+     * SIZE = 2*nhoodSize + 1
+     * because it will count for nhoodSize to the left, then nhoodSize to the
+     * right and finally, the particle itself;
+     * particles are assumed to be in circular order, so that particle 0 will
+     * have neighbours 1 and the last one.
+     */
+    int nhoodSize;
+
+    // boost serialization
+    friend class boost::serialization::access;
+
+    /*
+     * method that for serializing and deserializing objects of this class
+     * @param ar - archive stream
+     * @param version - don't know, not used
+     */
+    template<class archive>
+    void serialize(archive &ar, const unsigned int version) {
+        ar & boost::serialization::base_object<Neighbourhood>(*this);
+        ar & bestNeighbours;
+        ar & nhoodSize;
+    }
+
 public:
 
-    IndexedNeighbourhood(const Swarm& _swarm);
+    IndexedNeighbourhood();
+
+    IndexedNeighbourhood(int nParticles, int _nhoodSize);
 
     ~IndexedNeighbourhood();
 
     // Override
-    virtual void scanNeighbours();
+    virtual void scanNeighbours(const Swarm& swarm);
 
     // Override
-    virtual Particle& findBestNeighbour(int particleIndex) const; 
+    virtual const Particle& findBestNeighbour(int particleIndex) const; 
 };
 
 } // namespace PSO

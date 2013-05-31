61cd5e9d0078b173217f0ab4b123e7b8333f1b3c
islijepcevic <islijepcevic@gmail.com>
2013-05-31 20:31:06 +0200

    modified abstract n'hood

--- PSO/Neighbourhood.hpp
+++ PSO/Neighbourhood.hpp
@@ -9,33 +9,48 @@
 #ifndef __NEIGHBOURHOOD__
 #define __NEIGHBOURHOOD__
 
-#include <vector>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+#include "SwarmForward.hpp"
+#include "Particle.hpp"
 
 namespace PSO {
 
 class Neighbourhood {
-protected:
+private:
+    // BOOST SERIALIZATION
+    friend class boost::serialization::access;
 
-    Swarm& swarm;
+    /*
+     * method that for serializing and deserializing objects of this class
+     * @param ar - archive stream
+     * @param version - don't know, not used
+     */
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version) {
+    }
 
 public:
 
-    Neighbourhood(const Swarm& _swarm) : swarm(_swarm) {}
+    Neighbourhood() {
+    }
 
-    ~Neighbourhood();
+    ~Neighbourhood() {
+    }
 
     /*
      * scans all the necessary positions in the swarm, such that finding the
      * best neighbour can be fast
      */
-    virtual void scanNeighbours() = 0;
+    virtual void scanNeighbours(const Swarm& swarm) = 0;
 
     /*
      * finding the best evaluated neighbour for the given particle
      * @param particleIndex - index of a given particle in the swarm
-     * @return - reference to the best evaluated particle
+     * @return - index of the best evaluated particle in the neighbourhood
      */
-    virtual Particle& findBestNeighbour(int particleIndex) const = 0;
+    virtual const Particle& findBestNeighbour(int particleIndex) const = 0;
 };
 
 } // namespace PSO
